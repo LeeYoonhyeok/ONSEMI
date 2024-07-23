135627a992fd5ebb39185a4b5091031043ba1797
Polar-Bear-Poby <primekk3502@gmail.com>
2024-07-23 15:33:10 +0900

    feat: 보고서 목록의 필터 '신청자' 수정

--- management_app/views/report_list_views.py
+++ management_app/views/report_list_views.py
@@ -10,19 +10,17 @@ from django.http import JsonResponse
 @volunteer_required
 def report_list(request):
     user = request.user
-    sort_by = request.GET.get('sort_by', '-created_at')
-    volunteer_id = request.GET.get('volunteer_id', str(request.user.id))  # 현재 로그인한 봉사자의 ID로 기본값 설정
 
-    reports = Report.objects.all().order_by(sort_by)
+    # 작성 예정 보고서 초기 정렬 세팅 (방문 예정 날짜 : 최신순)
+    sort_by_pending = request.GET.get('sort_by_pending', '-visit_date')
 
-    # user_type이 'VOLUNTEER'인 봉사자 목록을 가져옴
-    volunteers = list(User.objects.filter(user_type='VOLUNTEER').exclude(id=request.user.id).order_by('username').values('id', 'username'))
-    current_user = {'id': request.user.id, 'username': request.user.username}
-    volunteers.insert(0, current_user)  # 현재 로그인한 사용자를 맨 앞에 추가
+    # 작성 완료 보고서 초기 정렬 세팅 (보고서 업데이트 날짜 : 최신순)
+    sort_by_approved = request.GET.get('sort_by_approved', '-updated_at')
 
-    reports = reports.filter(user_id=volunteer_id)
-    print('리포트 개수: ', reports.count())
-    print('리포트 id: ', reports.values_list('id', flat=True))
+    # 현재 로그인한 봉사자의 ID로 기본값 설정
+    volunteer_id = request.GET.get('volunteer_id', str(request.user.id))  
+
+    reports = Report.objects.filter(user_id=volunteer_id).order_by(sort_by_approved)
 
     seniors = Senior.objects.select_related('user_id').values('id', 'name', 'user_id', 'user_id__username')
 
@@ -39,7 +37,7 @@ def report_list(request):
     report_page_obj = report_paginator.get_page(report_page_number)
 
     # 작성해야할 보고서를 위한 페이지네이션
-    pending_cares = Care.objects.filter(care_state='APPROVED').exclude(id__in=Report.objects.values('care_id')).order_by('-datetime')
+    pending_cares = Care.objects.filter(care_state='APPROVED', approved_by=volunteer_id).order_by(sort_by_pending)
     pending_paginator = Paginator(pending_cares, 5)
     pending_page_number = request.GET.get('pending_page')
     pending_page_obj = pending_paginator.get_page(pending_page_number)
@@ -50,11 +48,11 @@ def report_list(request):
 
         'report_page_obj': report_page_obj,
         'pending_page_obj': pending_page_obj,
-        'sort_by': sort_by,
+        'sort_by_pending': sort_by_pending,
+        'sort_by_approved': sort_by_approved,
         'seniors': seniors,
         'users': User.objects.all(),
         'pending_reports_count': pending_reports_count,
-        'volunteers': volunteers,
         'unwritten_report_users': list(unwritten_report_users),
         'written_report_users': list(written_report_users),
     })
